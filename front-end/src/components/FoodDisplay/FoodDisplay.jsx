f57a16ed15e0098a6ea06342c46d10c4c92fd305
Basim Hilal V <basimhilalofficial@gmail.com>
2024-11-21 20:08:48 +0530

    corousal image update

--- front-end/src/components/FoodDisplay/FoodDisplay.jsx
+++ front-end/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,10 +2,11 @@ import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
+import { food_list } from '../../assets/assets'
 
 const FoodDisplay = ({category}) => {
 
-    const {food_list} = useContext(StoreContext)
+    // const {food_list} = useContext(StoreContext)
 
   return (
     <div className='food-display' id='food-display'>
